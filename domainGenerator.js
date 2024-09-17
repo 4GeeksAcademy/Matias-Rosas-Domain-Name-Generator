e38e3a7f7eca0857aaddd47df1acd39c5babf979
soycamaral <camaralr95@gmail.com>
2024-09-17 00:16:01 +0000

    Se eliminaron espacios innecesarios entre líneas. Cambié los nombres de las variables a inglés para acompañar a los elementos, y que pueda ser usado por más desarrolladores.

--- domainGenerator.js
+++ domainGenerator.js
@@ -1,12 +1,10 @@
-let marcas = ["tech", "sol", "group"];
+let companyNames = ["tech", "sol", "group"];
+let adjectives = ["fast", "smart", "secure"];
+let commercialDomains = [".com", ".net", ".org"];
 
-let adj = ["fast", "smart", "secure"];
-
-let dominioComercial = [".com", ".net", ".org"];
-
-for (let marca of marcas) {
-  for (let adjetivo of adj) {
-    for (let dominio of dominioComercial) {
+for (let marca of companyNames) {
+  for (let adjetivo of adjectives) {
+    for (let dominio of commercialDomains) {
       console.log("www." + marca + adjetivo + dominio);
     }
   }
